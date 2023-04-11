f7f3f544511897381b6890d4251123fd31638c73
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-04-12 01:15:23 +0900

    error message

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -5,7 +5,7 @@ class LinebotController < ApplicationController
         body = request.body.read
         signature = request.env['HTTP_X_LINE_SIGNATURE']
         unless client.validate_signature(body, signature)
-          error 400 do 'Bad Request' end
+          raise ActionController::BadRequest.new('Bad Request. validation error.')
         end
         events = client.parse_events_from(body)
 
